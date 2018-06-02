fd6b7be4ab99948208daee2221e5f02cb3c84e4f
victor <victor.nyagbe@gmail.com>
2018-06-02 09:51:53 +0000

    Modification du fichier en ajoutant le script permettant de dynamiser le champ etat

--- ajaxData.php
+++ ajaxData.php
@@ -7,4 +7,11 @@ if(!empty($_POST['country_id'])):
 
    $rowCount = $query->num_rows;
 
-   
+      if($rowCount > 0) :
+      echo '<option value="">Selectionner un etat</option>';
+      while($row = $query->fetch_assoc()):
+         echo'<option value="' .$row['state_id'] . '">' . $row['state_name'] . '</option>';
+      endwhile;
+   endif;
+endif;
+?>
